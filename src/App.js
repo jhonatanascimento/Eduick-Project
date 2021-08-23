192150891b836e15b550ae48f687d37e5c8e28dc
Jhonata Nascimento <87243089+jhonatanascimento@users.noreply.github.com>
2021-08-23 19:39:39 -0300

    Only LandingPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Home from "./components/landingpage/home/home";
-import Dashboard from "./components/dashboard/dashboard";
-
 export default function App() {
   return (
     <div className="app">
-      <Dashboard />
+      <Home />
     </div>
   );
 }
